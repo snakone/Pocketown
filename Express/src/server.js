32c7d587d6815bc59464b54ae36ffc33ccf1e3f4
Sergi <snak_one@hotmail.com>
2018-08-01 03:18:21 +0200

    Hello World from Express

--- Express/src/server.js
+++ Express/src/server.js
@@ -1,1 +1,25 @@
-console.log("hola");
+const express = require('express');  // Require Expres Module
+const cors = require('cors');
+const routes = require('./routes/routes');  // Require API Routes
+const app = express();  // Init Express on APP
+
+
+// Sever SETTING
+
+app.set('port', process.env.PORT || 3000);  // S.O Port or Port 3000
+app.engine('html', require('ejs').renderFile);  // Render HTML on Server
+app.set('view engine', 'ejs');
+
+// Middlewares
+
+app.use(cors());
+app.use(express.json());  // Body Parse to JSON
+
+// Routes
+
+app.use(routes);
+
+
+app.listen(app.get('port'), () => {  // Server listen on Port 3000
+  console.log('Server on Port', app.get('port'));
+})
