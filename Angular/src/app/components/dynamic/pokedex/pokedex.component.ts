24bc33cf411567c0c1cdd4d64b909607fd3b8505
Sergi <snak_one@hotmail.com>
2018-08-05 11:02:05 +0200

    Filter Pokemon

--- Angular/src/app/components/dynamic/pokedex/pokedex.component.ts
+++ Angular/src/app/components/dynamic/pokedex/pokedex.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { PokedexService } from '../../../services/pokedex.service';
 import { Pokemon } from '../../../models/pokemon';
@@ -12,9 +12,11 @@ import { Router } from '@angular/router'; // Router
 export class PokedexComponent implements OnInit {
 
   pokedex: Pokemon[];
+  filteredPokedex: Pokemon[];
   urlImage: string;
   selectedPokemon: Pokemon;
   loading: boolean;
+  searchValue: string = "";
 
   constructor(private pokedexService: PokedexService,
               private router: Router) {
@@ -27,8 +29,20 @@ export class PokedexComponent implements OnInit {
       .subscribe(res => {
         const pokemonList = res as Pokemon[];
         this.pokedex = pokemonList;
+        this.filteredPokedex = pokemonList;
         this.loading = false;
       })
+  }
+
+  onKeyUp(event){
+    event.preventDefault();
+    this.filterPokemon();
+  }
+
+  filterPokemon() {
+     this.filteredPokedex = this.pokedex.filter( ( pokemon, index ) => {
+      return pokemon.name.includes( this.searchValue);
+    } );
   }
 
   navigate(pokemon: Pokemon) {
