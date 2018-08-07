2cac7c5df88b9a6a657973d75f3601f8a7a2a6be
Sergi <snak_one@hotmail.com>
2018-08-07 20:34:53 +0200

    Template String :)

--- Express/src/controllers/laboratory.controller.js
+++ Express/src/controllers/laboratory.controller.js
@@ -6,7 +6,7 @@ const laboratoryCtrl = {};  // Create Object. We add Methods to it so We can use
 
 laboratoryCtrl.filterPokemon = async (req, res) => {
   // RegExp create Regular Expression Object from a string Pattern = Type
-    const filterType = new RegExp(`^${req.query.type}`);  //  Custom String ES6
+    const filterType = new RegExp(`^${req.query.type}`);  //  Template String ES6
     const pokemon = await pokemons.find({ $or: [{type: filterType},
                                                 {type2: filterType}]
     }).sort({ pokedex: + 1 });  // Sort Pokemon by Pokedex DESC
