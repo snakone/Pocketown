0a47c5779b65cc6124886737565655e85a9d417d
snakone <snak_one@hotmail.com>
2018-09-17 10:22:46 +0200

    Production

--- Express/src/controllers/trainer.controller.js
+++ Express/src/controllers/trainer.controller.js
@@ -24,9 +24,29 @@ trainerCtrl.getTrainerbyID = async (req, res) => {  // Get Trainer by ID
       res.json(trainer);  // Send Trainer to server as JSON
 }
 
+trainerCtrl.updateTrainer = async (req, res) => {  // Update Trainer
+
+   const { id } = req.params;  // From req.params only need Trainer ID
+   const trainer = req.body; // Trainer with updated INFO
+
+   await trainers.findByIdAndUpdate(id, {$set: trainer},  // Find by ID and Update in MongoDB
+                                     {new: true});  // New Trainer if ID not Match
+     res.json({
+       status: "Trainer Updated"
+     });
+}
+
+trainerCtrl.deleteTrainer = async (req, res) => {  // Retrainer Trainer from MongoDB
+
+  await trainers.findByIdAndRemove(req.params.id);  // Retrainer by ID
+  res.json({
+    status: "Trainer Deleted"
+  });
+}
+
 trainerCtrl.updateStatus = async (req, res) => {  // Update Status Online
 
-   const { id } = req.params;  // From req.params only need Move ID
+   const { id } = req.params;  // From req.params only need Trainer ID
    const status = req.body; // Status? Online or Offline
 
    await trainers.update({trainerID: id}, {$set: status},  // Find trainerID and Update in MongoDB
--- Express/src/routes/trainer-routes.js
+++ Express/src/routes/trainer-routes.js
@@ -7,8 +7,10 @@ const trainer = require('../controllers/trainer.controller');  // Controller
 // Trainer Routes
 
 trainerRouter.post('/trainer', trainer.addTrainer);  // Add New Trainer
-trainerRouter.put('/trainer/:id', trainer.updateStatus);  // Online Status
+trainerRouter.put('/trainer/status/:id', trainer.updateStatus);  // Online Status
 trainerRouter.get('/trainer', trainer.getTrainers);  // Get Trainers
 trainerRouter.get('/trainer/:id', trainer.getTrainerbyID);  // Get Trainer
+trainerRouter.put('/trainer/:id', trainer.updateTrainer);  // Update Trainer by ID
+trainerRouter.delete('/trainer/:id', trainer.deleteTrainer);  // Delete Trainer from MongoDB
 
 module.exports = trainerRouter;  // Export router - Export Class on TypeScript
